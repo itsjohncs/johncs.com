1379642835b730822d67046afc31bdd598e0f145
brownhead <jsull003@ucr.edu>
2014-03-06 17:13:12 -0800

    Front page now works.
    * Everything is all set now.

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from phial import *
+from phial import page, register_static_page, RenderedPage, Document, process
 from docutils.core import publish_parts
+from collections import namedtuple
 import pystache
+import os
 
 register_static_page("styles.css")
+
+# I'm going to collect the posts so I can put them on the front page. I'm just
+# going to store the frontmatter of each post cause that's all I need.
+posts = []
+
+def render_rst(text):
+    docutils_settings = {
+        # I don't want <h1> tags in the post
+        "initial_header_level": 2,
+
+        # I don't want the docutils class added to every element
+        "strip_classes": "docutils"
+    }
+    post_body = publish_parts(text, writer_name = "html",
+        settings_overrides = docutils_settings)["html_body"]
+
+    return post_body
 
 @page(files = "posts/*.rst")
 def post_page(source):
@@ -10,15 +29,42 @@ def post_page(source):
 
     # Figure out where we want the filename to be on the site (just replace
     # .rst with .htm).
-    output_file = os.path.basename(source.file_path)[:-3] + "htm"
+    output_file_name = os.path.basename(source.file_path)[:-3] + "htm"
+    output_file_path = "posts/{}".format(output_file_name)
 
+    # Save the frontmatter of the post so we can use it on the main page, also
+    # make note of where we're going to put it in the site.
+    source.frontmatter["link"] = output_file_path
+    posts.append(source.frontmatter)
+
+    # Use docutils to render the restructured text
+    post_body = render_rst(source.body)
+
+    # Use mustache to plug everything into the template
     renderer = pystache.Renderer()
-    post_body = publish_parts(source.body, writer_name = "html",
-        settings_overrides = {"initial_header_level": 2, "strip_classes": "docutils"})["html_body"]
     body = renderer.render(template.body, source.frontmatter,
         {"body": post_body})
 
-    return RenderedPage(body = body, path = "posts/{}".format(output_file))
+    return RenderedPage(body = body, path = output_file_path)
+
+@page(path = "index.htm")
+def main_page():
+    template = Document("index.htm")
+
+    # The posts we're going to show on the front page
+    displayed_posts = list(reversed(posts))
+    for i in displayed_posts:
+        i["description"] = render_rst(i["description"])
+
+    # Use mustache to plug everything into the template
+    renderer = pystache.Renderer()
+    body = renderer.render(template.body, {"posts": displayed_posts})
+
+    # We can just return the resulting string to phial since we told it the
+    # path already in the decorator.
+    return body
+
+
 
 if __name__ == "__main__":
     process()
