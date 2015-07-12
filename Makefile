72b30f41f69013279bd27920c0826ed25f07a3f9
brownhead <johnsullivan.pem@gmail.com>
2015-07-12 15:59:53 -0700

    Add LESS support.

--- Makefile
+++ Makefile
@@ -14,4 +14,4 @@ env:
 deps: env
 	env/bin/pip install --upgrade PyYAML pystache docutils pygments
 	bower install normalize.css
-	npm install gulp gulp-shell gulp-inject gulp-foreach gulp-concat gulp-minify-css gulp-minify-html gulp-imagemin gulp-webserver gulp-uncss
+	npm install gulp gulp-shell gulp-inject gulp-foreach gulp-concat gulp-minify-css gulp-minify-html gulp-imagemin gulp-webserver gulp-uncss gulp-less
--- gulpfile.js
+++ gulpfile.js
@@ -9,20 +9,22 @@ var minifyInline = require("gulp-minify-inline");
 var shell = require("gulp-shell");
 // var uncss = require('gulp-uncss');
 var webserver = require("gulp-webserver");
+var less = require("gulp-less");
 
 gulp.task("phial", shell.task([
     "rm -rf /tmp/johncs-phial",
     "PYTHONPATH=./phial python -m phial.__main__ --output /tmp/johncs-phial -v ./app.py",
 ]));
 
-function inline_css(html_glob, page_css_glob, output_dir) {
+function inline_css(html_glob, page_less_glob, output_dir) {
     return gulp.src(html_glob)
         .pipe(foreach(function(stream, file){
             // Gather all of the CSS we want to inline in this post
             var css = (gulp
-                .src([page_css_glob, "bower_components/normalize.css", "styles/common.css",
+                .src([page_less_glob, "bower_components/normalize.css",
                       "styles/pygments.css"])
-                .pipe(concat("all.css")))
+                .pipe(concat("all.less")))
+                .pipe(less())
                 // .pipe(uncss({
                 //     html: file.contents.toString("utf8")
                 // }))
@@ -43,8 +45,8 @@ function inline_css(html_glob, page_css_glob, output_dir) {
 }
 
 gulp.task("processed-pages", ["phial"], function() {
-    inline_css("/tmp/johncs-phial/posts/*", "styles/post-page.css", "output/posts/");
-    inline_css("/tmp/johncs-phial/index.htm", "styles/main-page.css", "output/");
+    inline_css("/tmp/johncs-phial/posts/*", "styles/post-page.less", "output/posts/");
+    inline_css("/tmp/johncs-phial/index.htm", "styles/main-page.less", "output/");
     gulp.src("/tmp/johncs-phial/rss.xml").pipe(gulp.dest("output"));
 });
 
