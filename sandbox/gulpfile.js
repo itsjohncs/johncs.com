a4fd279052c77b787cc02b2a28c7937b3279e69b
brownhead <johnsullivan.pem@gmail.com>
2015-11-22 17:26:09 -0800

    Inject CSS and JS into template file.
    This builds off the last commit and creates something actually useful.
    The gulp file will now *actually* crush the package down into one file
    that contains a concatenation of all the source files.
    
    Test Plan:
    Run `gulp crush` in `sandbox/`. Then run `cat build/crush-output/*' and
    see:
    
    ```
    <!-- Begin post.htm -->
    <head>
    	<!-- inject:head:css -->
    	<!-- endinject -->
    	<!-- inject:head:js -->
    	<script>console.log("hello");
    
    	console.log("world");
    	</script>
    	<!-- endinject -->
    </head>
    <body>
    
    </body>
    <!-- Begin index.htm -->
    <head>
    	<!-- inject:head:css -->
    	<style>#main {
    		color: black;
    	}
    	</style>
    	<!-- endinject -->
    	<!-- inject:head:js -->
    	<!-- endinject -->
    </head>
    <body>
    
    </body>
    ```

--- sandbox/gulpfile.js
+++ sandbox/gulpfile.js
@@ -1,7 +1,8 @@
-const gulp = require("gulp");
-const path = require("path");
-const foreach = require("gulp-foreach");
 const concat = require("gulp-concat");
+const foreach = require("gulp-foreach");
+const gulp = require("gulp");
+const inject = require("gulp-inject");
+const path = require("path");
  
 gulp.task("crush", function() {
     // A stream of all of our package descriptions
@@ -15,25 +16,45 @@ gulp.task("crush", function() {
         // Figure out the package's base path (the directory containing the package.json file)
         const basePath = path.dirname(file.path);
 
-        // Grab all the package's CSS files it wants to inline and process them
+        // Grab all the package's CSS files it wants to inline and create a stream that processes
+        // them.
         const relativeCssPaths = packageInfo["inline-css"] || [];
         const cssPaths = relativeCssPaths.map(function (relativePath) {
             return path.join(basePath, relativePath);
         });
         const cssStream = gulp.src(cssPaths).pipe(concat("all-css.css"));
 
-        // Similarily, grab all the package's JS files it wants to inline and process them
+        // Similarily, grab all the package's JS files it wants to inline and create a stream that
+        // processes them.
         const relativeJsPaths = packageInfo["inline-js"] || [];
         const jsPaths = relativeJsPaths.map(function (relativePath) {
             return path.join(basePath, relativePath);
         });
         const jsStream = gulp.src(jsPaths).pipe(concat("all-js.js"));
 
-        // Smash everything together into the template and do some aggregate processing on the
-        // whole thing.
-        const aggregateStream = gulp.src(path.join(basePath, packageInfo.template));
+        // Grab the template file and put it by itself into a new stream
+        const templateStream = gulp.src(path.join(basePath, packageInfo.template));
 
-        return aggregateStream.pipe(concat(packageInfo.target));
+        // Create a stream that injects our processed CSS and JS into the template file and yields
+        // a single file with the name we want.
+        const aggregateStream = templateStream
+            .pipe(inject(cssStream, {
+                starttag: "<!-- inject:head:css -->",
+                transform: function (filePath, file) {
+                    // return file contents as string 
+                    return "<style>" + file.contents.toString("utf-8") + "</style>";
+                }
+            }))
+            .pipe(inject(jsStream, {
+                starttag: "<!-- inject:head:js -->",
+                transform: function (filePath, file) {
+                    // return file contents as string 
+                    return "<script>" + file.contents.toString("utf-8") + "</script>";
+                }
+            }))
+            .pipe(concat(packageInfo.target));
+
+        return aggregateStream;
     }));
 
     return outputStream.pipe(gulp.dest("./build/crush-output/"));
