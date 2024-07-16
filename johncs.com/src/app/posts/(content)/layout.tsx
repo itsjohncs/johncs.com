85fe58209d31b018f1667cd574c66a16e785d50d
itsjohncs <johnsullivan.pem@gmail.com>
2024-07-16 16:55:00 -0700

    Add footer to posts.

---
+++ johncs.com/src/app/posts/(content)/layout.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export default function PostLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <main>{children}</main>
+      <footer>
+        <div aria-hidden={true}>--</div>
+        <Link href="/posts">« more posts</Link> | follow me via{" "}
+        <a href="https://twitter.com/itsjohncs">twitter</a> or{" "}
+        <Link href="/rss.xml">RSS</Link>
+      </footer>
+    </>
+  );
+}
