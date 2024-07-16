476e08db128038239b89f2b8f6ff6940195706ec
itsjohncs <johnsullivan.pem@gmail.com>
2024-07-16 11:34:47 -0700

    Make the mini-projects index JSX match template.
    I haven't styled it yet, but I believe the HTML it's generating is a
    close match now.

--- johncs.com/src/app/mini-projects/ProjectIndex.tsx
+++ johncs.com/src/app/mini-projects/ProjectIndex.tsx
@@ -5,20 +5,22 @@ import * as dieSim from '#root/app/mini-projects/projects/die-sim.mdx';
 import * as differentLogger from '#root/app/mini-projects/projects/different-logger.mdx';
 import * as tbget from '#root/app/mini-projects/projects/tbget.mdx';
 
-import styles from "./page.module.scss";
 import Link from 'next/link';
 
-interface ProjectMetadata {
-    title: string;
-    date: string;
-    url: string;
+function getSlug(rawURL: string): string {
+    try {
+        const url = new URL(rawURL);
+        const pathParts = url.pathname.split('/').filter(i => !!i);
+        if (pathParts.length === 0) {
+            throw new Error('URL path is empty');
+        }
+        return pathParts[pathParts.length - 1];
+    } catch (error) {
+      throw new Error('Invalid URL: ' + rawURL);
+    }
 }
 
-interface Project {
-    metadata: ProjectMetadata;
-}
-
-const projects: Project[] = [
+const projects = [
     simpleFrontmatter,
     dieSim,
     differentLogger,
@@ -26,16 +28,30 @@ const projects: Project[] = [
 ];
 
 export default function ProjectIndex() {
-    return <div className={styles.projectList}>
-        {projects.map((project: Project, index: number) => (
-              <div key={index} className={styles.project}>
-                <h2>
-                  <Link href={project.metadata.url}>
-                    {project.metadata.title}
-                  </Link>
-                </h2>
-                <p>{project.metadata.date}</p>
-              </div>
-        ))}
-    </div>
+    return (
+        <div className="all-posts">
+            {projects.map((project, index: number) => (
+                <article key={index} className="post">
+                    <div className="post-heading">
+                        <span>
+                            <Link href={project.metadata.url} className="post-title">
+                                {project.metadata.title}
+                            </Link>
+                            <time className="post-date">
+                                <span aria-hidden="true">[
+                                    <a id={getSlug(project.metadata.url)} className="timestamp-anchor" href={`#${getSlug(project.metadata.url)}`}>
+                                        {project.metadata.date}
+                                    </a>
+                                ]</span>
+                                <span className="sr-only">{project.metadata.date}</span>
+                            </time>
+                        </span>
+                    </div>
+                    <div className="post-body">
+                        <project.default />
+                    </div>
+                </article>
+            ))}
+        </div>
+    );
 }
