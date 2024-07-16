f09b494b8adda71fbf7d36469ed678bbba3caf18
itsjohncs <johnsullivan.pem@gmail.com>
2024-07-16 11:57:12 -0700

    Remove remaining AI created styling.

--- johncs.com/src/app/mini-projects/page.tsx
+++ johncs.com/src/app/mini-projects/page.tsx
@@ -1,10 +1,9 @@
-import styles from "./page.module.scss";
 import Link from 'next/link';
 import ProjectIndex from "./ProjectIndex";
 
 export default function MiniProjects() {
   return (
-    <main className={styles.main}>
+    <main>
       <h1>Mini Projects</h1>
       <p>Here are some of the (roughly) one-night projects I've made. Quality not at all guaranteed.</p>
       <ProjectIndex />
