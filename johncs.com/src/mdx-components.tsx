a9b5890d28aa175e145c9fffe84b94d3010415f2
itsjohncs <johnsullivan.pem@gmail.com>
2024-07-16 14:05:31 -0700

    Replaced custom code highlighting with a reusable CodeBlock component.

--- johncs.com/src/mdx-components.tsx
+++ johncs.com/src/mdx-components.tsx
@@ -1,35 +1,13 @@
 import type { MDXComponents } from "mdx/types";
-import { Highlight, themes } from "prism-react-renderer";
+import { CodeBlock } from "../components/CodeBlock";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     code: ({ children, className }) => {
-      if (!className || typeof children !== "string") {
+      if (typeof children !== "string") {
         return <code>{children}</code>;
-      } else {
-        const language = className.replace(/language-/gm, "");
-        return (
-          <Highlight theme={themes.vsLight} code={children} language={language}>
-            {({
-              className: highlightClassName,
-              style,
-              tokens,
-              getLineProps,
-              getTokenProps,
-            }) => (
-              <code className={highlightClassName} style={style}>
-                {tokens.map((line, i) => (
-                  <div key={i} {...getLineProps({ line })}>
-                    {line.map((token, key) => (
-                      <span key={key} {...getTokenProps({ token })} />
-                    ))}
-                  </div>
-                ))}
-              </code>
-            )}
-          </Highlight>
-        );
       }
+      return <CodeBlock className={className}>{children}</CodeBlock>;
     },
     ...components,
   };
