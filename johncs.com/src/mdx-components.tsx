5980ef6778d3eddc8b2eb5b26ba8d65e323fbdb8
itsjohncs <johnsullivan.pem@gmail.com>
2024-07-17 11:54:38 -0700

    Added custom heading components to the MDX components.

--- johncs.com/src/mdx-components.tsx
+++ johncs.com/src/mdx-components.tsx
@@ -1,9 +1,23 @@
 import type { MDXComponents } from "mdx/types";
 import { CodeBlock, CodeBlockBody } from "./mdx/CodeBlock";
 import { SuperScript, SuperScriptLink } from "./mdx/SuperScript";
+import {
+  Heading1,
+  Heading2,
+  Heading3,
+  Heading4,
+  Heading5,
+  Heading6,
+} from "./mdx/headings";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: Heading1,
+    h2: Heading2,
+    h3: Heading3,
+    h4: Heading4,
+    h5: Heading5,
+    h6: Heading6,
     pre: CodeBlock,
     code: CodeBlockBody,
     sup: SuperScript,
