428b875f72ee0ce4291422ffc3ea66670963b685
itsjohncs <johnsullivan.pem@gmail.com>
2024-07-17 14:51:47 -0700

    Sort posts by date.

--- johncs.com/src/app/posts/PostIndex.tsx
+++ johncs.com/src/app/posts/PostIndex.tsx
@@ -36,6 +36,11 @@ const postMetadata = [
   },
 ];
 
+// Sort posts by date, newest first
+const sortedPostMetadata = postMetadata.sort(
+  (a, b) => Date.parse(b.date) - Date.parse(a.date),
+);
+
 export default function PostIndex() {
   return postMetadata.map(function (metadata) {
     return (
