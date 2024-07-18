b3c59602a72cd740829fd72f4a3216a4c6a13758
itsjohncs <johnsullivan.pem@gmail.com>
2024-07-18 11:28:17 -0700

    Make a simple front-page blurb.

--- johncs.com/src/app/page.tsx
+++ johncs.com/src/app/page.tsx
@@ -1,13 +1,25 @@
 import NavBar from "#root/shared-components/NavBar";
 import styles from "./page.module.scss";
 import Link from "next/link";
+import { ReactNode } from "react";
+
+function LinkListItem({ children }: { children: ReactNode }) {
+    return (
+        <div className={styles.linkListItem} role="listitem">
+            <span className={styles.bullet} aria-hidden={true}>
+                {" * "}
+            </span>
+            {children}
+        </div>
+    );
+}
 
 export default function Home() {
     return (
         <>
             <NavBar path="/" />
             <main className={styles.main}>
-                <div className={styles.floatingHead}>
+                <div className={styles.selfieCell}>
                     <Link href="/me.jpg">
                         <img
                             src="/me.jpg"
@@ -16,30 +28,52 @@ export default function Home() {
                         />
                     </Link>
                 </div>
-                <div>
-                    <p>
-                        Welcome to my corner of the internet! I put silly things
-                        here like{" "}
-                        <Link href="./blog-index.htm">blog posts</Link> and{" "}
-                        <Link href="./mini-projects-index.htm">
-                            itty-bitty projects
-                        </Link>
-                        .
-                    </p>
-                    <p>
-                        I used to work at{" "}
-                        <a href="https://www.khanacademy.org">Khan Academy</a>{" "}
-                        but now I&apos;m doing some other stuff you can read
-                        about at <a href="http://johncs.com">johncs.com</a>. I
-                        spend my free time juggling, practicing magic tricks,
-                        swinging around on silks, and DMing D&D.
-                    </p>
-                    <p>
-                        Looking for more info on me? Check out my{" "}
-                        <a href="http://johnsullivan.name">resume-portfolio</a>,{" "}
-                        <a href="http://github.com/itsjohncs">GitHub profile</a>
-                        , or <a href="http://twitter.com/itsjohncs">twitter</a>.
-                    </p>
+                <div className={styles.aboutMe}>
+                    <ul className={styles.flatList}>
+                        <li>
+                            <b>John C Sullivan</b>
+                        </li>
+                        <li>
+                            Lead SWE & Founder @{" "}
+                            <a href="https://shmeppy.com">Shmeppy</a>
+                        </li>
+                        <li>
+                            Senior SWE @{" "}
+                            <a href="https://khanacademy.org">Khan Academy</a>{" "}
+                            (Former)
+                        </li>
+                    </ul>
+                    <div className={styles.linkList} role="list">
+                        <LinkListItem>
+                            <a href="mailto:johnsullivan.pem@gmail.com">
+                                Email
+                            </a>
+                        </LinkListItem>
+                        <LinkListItem>
+                            <a href="https://github.com/itsjohncs">GitHub</a>
+                        </LinkListItem>
+                        <LinkListItem>
+                            <a href="https://www.linkedin.com/in/john-sullivan-7696b621a/">
+                                LinkedIn
+                            </a>
+                        </LinkListItem>
+                        <LinkListItem>
+                            <a href="https://x.com/itsjohncs">X</a>
+                        </LinkListItem>
+                        <LinkListItem>
+                            <a href="https://tech.lgbt/@johncs">Mastodon</a>
+                        </LinkListItem>
+                        <LinkListItem>
+                            <a href="https://resume.johncs.com">Resume</a> (
+                            <a href="https://resume.johncs.com/resume.pdf">
+                                PDF
+                            </a>
+                            )
+                        </LinkListItem>
+                        <LinkListItem>
+                            <a href="https://lichess.org/@/johncs">Lichess</a>
+                        </LinkListItem>
+                    </div>
                 </div>
             </main>
         </>
