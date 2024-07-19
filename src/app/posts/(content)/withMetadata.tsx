959be6b59bac7de316897e20e8188959c8f88636
itsjohncs <johnsullivan.pem@gmail.com>
2024-07-18 19:13:02 -0700

    Add link to post source code.

--- src/app/posts/(content)/withMetadata.tsx
+++ src/app/posts/(content)/withMetadata.tsx
@@ -1,6 +1,19 @@
 import PostMetadata from "./PostMetadata";
 import { PostNavBar } from "#root/shared-components/NavBar";
 import { ReactNode } from "react";
+import Link from "next/link";
+
+function generateGitHubUrl(partialUrl: string): string {
+    // Extracts `slug` out of `/posts/slug`
+    const match = partialUrl.match(/^\/posts\/([a-z0-9-]+)$/);
+    if (!match) {
+        throw new Error("Invalid partial URL format. Expected: /posts/slug");
+    }
+
+    const slug = match[1];
+
+    return `https://github.com/itsjohncs/johncs.com/blob/main/src/app/posts/(content)/${slug}/page.mdx?plain=1`;
+}
 
 export default function withMetadata(metadata: PostMetadata) {
     return function post({ children }: { children: ReactNode }) {
@@ -11,7 +24,14 @@ export default function withMetadata(metadata: PostMetadata) {
                     date={metadata.post.date}
                     href={metadata.post.href}
                 />
-                {children}
+                <main>{children}</main>
+                <footer>
+                    <div aria-hidden={true}>--</div>
+                    <Link href="/posts">« more posts</Link> | follow me via{" "}
+                    <a href="https://x.com/itsjohncs">X</a> or{" "}
+                    <Link href="/rss.xml">RSS</Link> |{" "}
+                    <a href={generateGitHubUrl(metadata.post.href)}>source</a>
+                </footer>
             </>
         );
     };
