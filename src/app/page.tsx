a405a55c1d6df8021adcc02ce80f27223705c06f
itsjohncs <johnsullivan.pem@gmail.com>
2024-09-27 18:05:40 -0700

    Remove job blurb on frontpage.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,59 +67,6 @@ export default function Home() {
                         </LinkListItem>
                     </div>
                 </div>
-                <div className={styles.bio}>
-                    <p>
-                        In 2004 I tried some exercises out of{" "}
-                        <a href="https://books.google.com/books/about/Teach_Yourself_Visual_Basic_5_in_24_Hour.html?id=xCLwe6CY6zwC">
-                            an old Visual Basic 5 book
-                        </a>{" "}
-                        and was amazed at what I could create through coding.
-                        Fast-forward to today, and I&apos;ve had the privilege
-                        of building software for{" "}
-                        <a href="https://khanacademy.org">
-                            an ed-tech non-profit
-                        </a>
-                        ,{" "}
-                        <a href="https://www.shmeppy.com/">
-                            my own bootstrapped startup
-                        </a>
-                        ,{" "}
-                        <a href="https://www.mozilla.org/">
-                            an open-source browser
-                        </a>
-                        ,{" "}
-                        <a href="https://www.jetheaddev.com/">
-                            a set-top box manufacturer
-                        </a>
-                        ,{" "}
-                        <a href="https://www.youtube.com/c/PrimerLearning">
-                            an educational YouTuber
-                        </a>
-                        ,{" "}
-                        <a href="https://www.color-track.com/">
-                            a quality-control hardware company
-                        </a>
-                        , and more.
-                    </p>
-                    <p>
-                        Now I&apos;m on the job market! I&apos;m looking for
-                        senior/staff+ level software engineering roles,
-                        especially those that:
-                    </p>
-                    <UnorderedList>
-                        <li>Have diverse, kind, and hyper-competent teams.</li>
-                        <li>Pay well.</li>
-                        <li>Are remote or based in Portland, Oregon.</li>
-                        <li>Have an outsized impact on many users.</li>
-                    </UnorderedList>
-                    <p>
-                        Please{" "}
-                        <a href="mailto:johnsullivan.pem@gmail.com">
-                            reach out
-                        </a>{" "}
-                        if you have a role in mind for me.
-                    </p>
-                </div>
             </main>
         </>
     );
